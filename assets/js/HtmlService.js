d4296baffe7f784e8c6fa1cdc6b8ac423decb58d
Edy Segura <edysegura@gmail.com>
2020-11-28 15:33:10 -0300

    Implement new task additional

--- assets/js/HtmlService.js
+++ assets/js/HtmlService.js
@@ -9,14 +9,40 @@ export default class HtmlService {
     const form = document.querySelector("form");
     form.addEventListener("submit", (event) => {
       event.preventDefault();
-      console.log("submitted! Value: " + form.item.value);
+      this.addTask(form.item.value);
       form.reset();
       form.item.focus();
     });
   }
 
+  async addTask(description) {
+    const task = { description, done: false };
+    const taskId = await this.todoService.save(task);
+    task.id = taskId;
+    this.addToHtmlList(task);
+  }
+
   async listTasks() {
     const tasks = await this.todoService.getAll();
-    console.log(tasks);
+    tasks.forEach((task) => this.addToHtmlList(task));
+  }
+
+  addToHtmlList(task) {
+    const ul = document.querySelector("ul");
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+
+    li.setAttribute("data-item-id", task.id);
+    span.textContent = task.description;
+    button.textContent = "x";
+
+    if (task.done) {
+      li.classList.add("done");
+    }
+
+    li.appendChild(span);
+    li.appendChild(button);
+    ul.appendChild(li);
   }
 }
