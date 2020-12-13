e0c90b8d48eebd7380fbd0df899a0e29486cfcd5
Samuel Pereira <samuelpereira@gec.inatel.br>
2020-12-12 21:10:44 -0300

    refactor app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,21 @@
-'use strict'
+class App {
 
-if ('serviceWorker' in navigator) {
-  const onsuccess = () => console.log('[Service Worker] Registered');
-  const onfailure = () => console.log('[Service Worker] Failed');
+  constructor() {
+    this.registerServiceWorker();
+  }
 
-  navigator.serviceWorker
-    .register('sw.js')
-    .then(onsuccess)
-    .catch(onfailure);
+  registerServiceWorker() {
+    if ('serviceWorker' in navigator) {
+      const onsuccess = () => console.log('[Service Worker] Registered');
+      const onfailure = () => console.log('[Service Worker] Failed');
+    
+      navigator.serviceWorker
+        .register('sw.js')
+        .then(onsuccess)
+        .catch(onfailure);
+    }
+  }
 }
+
+new App();
+
